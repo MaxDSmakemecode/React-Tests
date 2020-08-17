e4d7f24e2a65360fc316a29f827c7b6aae0700d3
Daisuke <mds87@hotmail.com>
2020-08-17 15:28:14 +0200

    changed App function to class

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import MyFirstComponent from './components/myFirstComponent';
 import ParentComponent from './components/parentComponent.jsx';
 
 // Firt comment in development branch for git
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyFirstComponent />
-        <ParentComponent />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <MyFirstComponent />
+          <ParentComponent />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
